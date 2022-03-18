8b157991fef533bafdbfc607a67ac22e682c4423
Cist1 <fcova89@gmail.com>
2022-03-18 05:54:31 +0100

    es1.1

--- myApp.js
+++ myApp.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
-var mongoose = require('mongoose');
+//var mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+//mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 
 let Person;
