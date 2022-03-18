efcf26d2b3cdd95fc02e76a6f2654be9958ce8e3
Cist1 <fcova89@gmail.com>
2022-03-18 05:58:54 +0100

    arrivato fino ad es 3

--- myApp.js
+++ myApp.js
@@ -1,10 +1,18 @@
 require('dotenv').config();
-//var mongoose = require('mongoose');
+var mongoose = require('mongoose');
 
-//mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
+const personSchema = new mongoose.Schema({ //creo Schema per un modello
+  name: { 
+    type: String,
+    required: true},
+  age: Number,
+  favoriteFoods: [String]
+});
 
-let Person;
+let Person = mongoose.model('Person', personSchema); //assegno a Person il modello basato sullo schema PersonShcema
+
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
