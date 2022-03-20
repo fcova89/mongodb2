529daf67ceb73f5427ca41186932f5899c992880
Cist1 <fcova89@gmail.com>
2022-03-20 18:53:36 +0100

    test con find & sort4

--- myApp.js
+++ myApp.js
@@ -173,16 +173,10 @@ const removeManyPeople = (done) => {
 
 const queryChain = (done) => {
   const foodToSearch = "burrito";
-  Person.find({name: personName},function (err,foundPeople) {//estrai persone a cui piace il burrito
-    console.log(`Le persone a cui piace il ${foodToSearch} sono ` + foundPeople);
-    if (err) {
-      console.log('Errore da qualche parte' + err);
-      return done(err)
-    }
-    foundPeople.sort({name : 'asc'}) //metti in ordine ascendente?
-    console.log('Le persone in ordine sono '+ foundPeople);
-    done(null, foundPeople);
-  })
+  var foundPerson = Person.find({name: personName}).exec()
+  console.log(foundPerson);
+  done (null, foundPerson);
+  //foundPerson.sort({name : 'asc'})
 };
 
 /** **Well Done !!**
