71c5d2c3fd95114886082162f4b1c36c1914afa2
Cist1 <fcova89@gmail.com>
2022-03-20 18:40:53 +0100

    test con find & sort2

--- myApp.js
+++ myApp.js
@@ -174,7 +174,7 @@ const removeManyPeople = (done) => {
 const queryChain = (done) => {
   const foodToSearch = "burrito";
   Person.find({favoriteFoods: [foodToSearch]},function (err,foundPeople) {//estrai persone a cui piace il burrito
-    console.log(`Le persone a cui piace il ${foodToSearch} sono ${foundPeople}`);
+    console.log(`Le persone a cui piace il ${foodToSearch} sono ` + foundPeople);
     if (err) {
       return done(err)
     }
